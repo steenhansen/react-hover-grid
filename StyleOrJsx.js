cefc61c40e42cd77f2f66de57c19cf9a81c0bcbd
steen hansen <steen_hansen@yahoo.com>
2017-09-04 22:55:51 -0700

    changed structure

--- StyleOrJsx.js
+++ StyleOrJsx.js
@@ -1,52 +1,49 @@
-//'use strict'
+'use strict'
 
-var StyleOrJsx = (function () {
+function StyleOrJsx(className_list, jsx_styles) {
+  className_list = (typeof className_list !== 'undefined') ? className_list : ''
+  jsx_styles = (typeof jsx_styles !== 'undefined') ? jsx_styles : {}
+  var members = Object.assign({}, {className_list:className_list, jsx_styles:jsx_styles})
 
-   Constr = function (className_list, jsx_styles) {
-    className_list = (typeof className_list !== 'undefined') ? className_list : ''
-    jsx_styles = (typeof jsx_styles !== 'undefined') ? jsx_styles : {}
-    this.className_list = className_list
-    this.jsx_styles = jsx_styles
-  }
-
-  Constr.prototype = {
-
-    addStyling: function (className_or_jsx) {
-      if (typeof className_or_jsx === 'undefined') {
-        return ''
-      } else if (typeof className_or_jsx === 'object') {
-        this.jsx_styles = Object.assign(this.jsx_styles, className_or_jsx)
-      } else if (typeof className_or_jsx === 'string' || className_or_jsx instanceof String) {
-        if (className_or_jsx.includes(':')) {
-        } else {
-          if (className_or_jsx.charAt(0) === '.') {
-            className_or_jsx = className_or_jsx.substr(1)
-          }
-          this.className_list += ' ' + className_or_jsx
+ let addStyling = function (className_or_jsx) {
+    if (typeof className_or_jsx === 'undefined') {
+      return ''
+    } else if (typeof className_or_jsx === 'object') {
+      members.jsx_styles = Object.assign(members.jsx_styles, className_or_jsx)
+    } else if (typeof className_or_jsx === 'string' || className_or_jsx instanceof String) {
+      if (className_or_jsx.includes(':')) {
+      } else {
+        if (className_or_jsx.charAt(0) === '.') {
+          className_or_jsx = className_or_jsx.substr(1)
         }
-      }
-    }
-
-    , collectedJsx: function () {
-      if (Object.keys(this.jsx_styles).length === 0) {
-        return {}
-      } else {
-        return this.jsx_styles
-      }
-    }
-
-    , collectedClassNames: function () {
-      const trimmed_classNames = this.className_list.trim()  
-      if (trimmed_classNames.length === 0) {
-        return ''
-      } else {
-        return trimmed_classNames
+        members.className_list += ' ' + className_or_jsx
       }
     }
   }
-  return Constr
 
-})()
+ let collectedJsx = function () {
+    if (Object.keys(members.jsx_styles).length === 0) {
+      return {}
+    } else {
+      return members.jsx_styles
+    }
+  }
+
+  let collectedClassNames = function () {
+    const trimmed_classNames = members.className_list.trim()
+    if (trimmed_classNames.length === 0) {
+      return ''
+    } else {
+      return trimmed_classNames
+    }
+  }
+
+  return Object.freeze({
+    addStyling: addStyling,
+    collectedJsx: collectedJsx,
+    collectedClassNames:collectedClassNames
+  });
+}
 
 module.exports = StyleOrJsx
 
